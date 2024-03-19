0af8c1f71516e8101fbaade63f35f0fcf96c8f1f
martapla <martaplanella@hotmail.com>
2024-03-19 12:20:14 +0100

    redux

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,9 +7,9 @@ const Header = () => {
     <header>
        <h1> Redux toolkit Example</h1>   
           <ul>
-              <li>Name:{user.name }</li>
-              <li>Email: </li>
-              <li>Username: </li>
+            <li>Name: {user.name}</li>
+            <li>Username: {user.username}</li>
+            <li>Email: {user.email}</li>
           </ul>
     </header>
   )
